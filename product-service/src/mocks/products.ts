73ef3de3252d84ce8ffc5700e1c23b3b0437e144
Val-d-emar <vhot@live.ru>
2026-05-01 15:51:51 +0400

    feat: add apiResponse

--- product-service/src/mocks/products.ts
+++ product-service/src/mocks/products.ts
@@ -1,4 +1,3 @@
-// src/mocks/products.ts
 export const products =[
   {
     id: '7567ec4b-b10c-48c5-9345-fc73c48a80aa',
---
+++ product-service/src/utils/apiResponse.ts
@@ -0,0 +1,11 @@
+export const formatJSONResponse = (statusCode: number, response: Record<string, unknown> | Array<unknown> | string) => {
+  return {
+    statusCode,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // for CORS
+      'Access-Control-Allow-Credentials': true,
+      'Content-Type': 'application/json',
+    },
+    body: typeof response === 'string' ? response : JSON.stringify(response),
+  };
+};
