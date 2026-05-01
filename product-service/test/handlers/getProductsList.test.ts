4f5ecfa40901ff5107cce33851db318f6bfc7645
Val-d-emar <vhot@live.ru>
2026-05-01 20:32:25 +0400

    tests: PASS  getProductsList.test

--- product-service/test/handlers/getProductsList.test.ts
+++ product-service/test/handlers/getProductsList.test.ts
@@ -3,7 +3,7 @@ import { products } from "../../src/mocks/products";
 
 describe("getProductsList handler", () => {
   it("should return 200 and all products", async () => {
-    const result = await handler({} as any); // Эмулируем пустой event
+    const result = await handler({} as any);
 
     expect(result.statusCode).toBe(200);
     expect(JSON.parse(result.body)).toEqual(products);
