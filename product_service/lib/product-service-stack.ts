e8d6020b56b9d5510189826e0271640edf939ec6
Val-d-emar <vhot@live.ru>
2026-05-08 20:21:29 +0400

    refactor:  product-service-stack

--- product_service/lib/product-service-stack.ts
+++ product_service/lib/product-service-stack.ts
@@ -4,28 +4,48 @@ import * as lambda from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as path from "path";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 
 export class ProductServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const getProductsList = new NodejsFunction(this, "GetProductsList", {
+    const productsTable = dynamodb.Table.fromTableName(
+      this,
+      "ProductsTable",
+      "products",
+    );
+    const stocksTable = dynamodb.Table.fromTableName(
+      this,
+      "StocksTable",
+      "stocks",
+    );
+
+    const lambdaProps = {
       runtime: lambda.Runtime.NODEJS_20_X,
       environment: {
-        PRODUCT_AWS_REGION: cdk.Stack.of(this).region,
+        PRODUCTS_TABLE: productsTable.tableName,
+        STOCKS_TABLE: stocksTable.tableName,
       },
+    };
+
+    const getProductsList = new NodejsFunction(this, "GetProductsList", {
+      ...lambdaProps,
       entry: path.join(__dirname, "../src/handlers/getProductsList.ts"),
       handler: "handler",
     });
 
     const getProductsById = new NodejsFunction(this, "GetProductsById", {
-      runtime: lambda.Runtime.NODEJS_20_X,
-      environment: {
-        PRODUCT_AWS_REGION: cdk.Stack.of(this).region,
-      },
+      ...lambdaProps,
       entry: path.join(__dirname, "../src/handlers/getProductsById.ts"),
       handler: "handler",
     });
+
+    productsTable.grantReadData(getProductsList);
+    stocksTable.grantReadData(getProductsList);
+
+    productsTable.grantReadData(getProductsById);
+    stocksTable.grantReadData(getProductsById);
 
     const api = new apigateway.RestApi(this, "ProductsApi", {
       restApiName: "Product Service",
