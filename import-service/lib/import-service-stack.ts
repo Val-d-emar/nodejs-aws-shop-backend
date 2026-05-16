78ff75daeff36763393ced833f8f0237d9f040c3
Val-d-emar <vhot@live.ru>
2026-05-16 15:59:43 +0400

    feat: ImportServiceStack

--- import-service/lib/import-service-stack.ts
+++ import-service/lib/import-service-stack.ts
@@ -1,16 +1,58 @@
-import * as cdk from 'aws-cdk-lib/core';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as path from "path";
 
 export class ImportServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucketName = "rs-shop-import-val-d-emar-2026";
+    const importBucket = s3.Bucket.fromBucketName(
+      this,
+      "ImportBucket",
+      bucketName,
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ImportServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const importProductsFile = new NodejsFunction(this, "ImportProductsFile", {
+      runtime: lambda.Runtime.NODEJS_24_X,
+      entry: path.join(__dirname, "../src/handlers/importProductsFile.ts"),
+      handler: "handler",
+      environment: {
+        UPLOAD_BUCKET: bucketName,
+      },
+    });
+
+    importBucket.grantWrite(importProductsFile);
+
+    const api = new apigateway.RestApi(this, "ImportApi", {
+      restApiName: "Import Service",
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: apigateway.Cors.ALL_METHODS,
+        allowHeaders: [
+          "Content-Type",
+          "Authorization",
+          "X-Amz-Date",
+          "X-Api-Key",
+          "X-Amz-Security-Token",
+        ],
+      },
+      binaryMediaTypes: ["multipart/form-data"],
+    });
+
+    const importResource = api.root.addResource("import");
+    importResource.addMethod(
+      "GET",
+      new apigateway.LambdaIntegration(importProductsFile),
+      {
+        requestParameters: {
+          "method.request.querystring.name": true,
+        },
+      },
+    );
   }
 }
