cd1d3ef5b03b935508948885e75107ad2ffeeddf
Val-d-emar <vhot@live.ru>
2026-05-30 19:59:52 +0400

    feat: AuthorizationServiceStack

--- authorization-service/lib/authorization-service-stack.ts
+++ authorization-service/lib/authorization-service-stack.ts
@@ -1,16 +1,31 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import * as path from "path";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 export class AuthorizationServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const testCredentials = process.env.TEST_CREDENTIALS || '{}';
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AuthorizationServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const basicAuthorizer = new NodejsFunction(this, 'BasicAuthorizer', {
+      runtime: lambda.Runtime.NODEJS_24_X,
+      entry: path.join(__dirname, '../src/handlers/basicAuthorizer.ts'),
+      handler: 'handler',
+      environment: {
+        TEST_CREDENTIALS: testCredentials,
+      },
+    });
+
+    new cdk.CfnOutput(this, 'BasicAuthorizerArn', {
+      value: basicAuthorizer.functionArn,
+      exportName: 'BasicAuthorizerArn',
+    });
   }
+
 }
