884b82443ed47f8e03ae2754fa433a72cb78d598
Val-d-emar <vhot@live.ru>
2026-06-20 23:10:07 +0400

    feat: add fastify server, it works

---
+++ bff-service/src/index.ts
@@ -0,0 +1,90 @@
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import axios, { AxiosError } from "axios";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const fastify = Fastify({
+  logger: true,
+});
+
+const PORT = process.env.PORT || 3000;
+
+fastify.register(cors, {
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+});
+
+fastify.route({
+  method: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  url: "/*",
+  handler: async (request, reply) => {
+    const url = request.url;
+
+    const parts = url.split("/").filter(Boolean);
+    const recipient = parts[0];
+
+    if (!recipient) {
+      return reply
+        .status(400)
+        .send("Bad Request: Missing recipient service name.");
+    }
+
+    const recipientUrl = process.env[recipient];
+
+    if (!recipientUrl) {
+      return reply
+        .status(502)
+        .send("Cannot process request: Recipient service not found.");
+    }
+
+    const remainingPath = "/" + parts.slice(1).join("/");
+    const targetUrl = `${recipientUrl}${remainingPath}`;
+
+    const headers = { ...request.headers };
+    delete headers.host;
+
+    try {
+      fastify.log.info(`Forwarding request to: ${targetUrl}`);
+
+      const response = await axios({
+        method: request.method,
+        url: targetUrl,
+        headers: headers as any,
+        data: request.body,
+      });
+
+      return reply.status(response.status).send(response.data);
+    } catch (error) {
+      const axiosError = error as AxiosError;
+
+      if (axiosError.response) {
+        fastify.log.error(
+          `Error from recipient service: ${axiosError.response.status}`,
+        );
+        return reply
+          .status(axiosError.response.status)
+          .send(axiosError.response.data);
+      }
+
+      fastify.log.error(axiosError, `Connection error to: ${targetUrl}`);
+      return reply
+        .status(500)
+        .send({ message: "Internal server error", error: axiosError.message });
+    }
+  },
+});
+
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: Number(PORT), host: "0.0.0.0" });
+    console.log(`BFF Service is running on port ${PORT}`);
+  } catch (err) {
+    fastify.log.error(err as Error);
+    process.exit(1);
+  }
+};
+
+start();
