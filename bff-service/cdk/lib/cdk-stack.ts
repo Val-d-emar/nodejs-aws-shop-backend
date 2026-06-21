64fcf6449c8a30f82d0ccb6d6afb66c0f35a3731
Val-d-emar <vhot@live.ru>
2026-06-21 13:42:13 +0400

    feat: implemented BffServiceStack

--- bff-service/cdk/lib/cdk-stack.ts
+++ bff-service/cdk/lib/cdk-stack.ts
@@ -1,16 +1,41 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import process from 'node:process';
+import { config as dotenv } from 'dotenv';
 
+dotenv();
+ 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const ebDomain = process.env.BFF_EB_URL || "";
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const distribution = new cloudfront.Distribution(
+      this,
+      "BffApiDistribution",
+      {
+        defaultBehavior: {
+          origin: new origins.HttpOrigin(ebDomain, {
+            protocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
+          }),
+          viewerProtocolPolicy:
+            cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+          allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
+          cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED,
+          originRequestPolicy:
+            cloudfront.OriginRequestPolicy.ALL_VIEWER_EXCEPT_HOST_HEADER,
+        },
+      },
+    );
+
+    new cdk.CfnOutput(this, "env.BFF_EB_URL", {
+      value: `${ebDomain}`,
+    });
+    new cdk.CfnOutput(this, "BffCloudFrontUrl", {
+      value: `https://${distribution.distributionDomainName}`,
+    });    
   }
 }
